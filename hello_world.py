d9fe22e553e8a7b438cd2b0626242386915fb43b
Paul Giese <giese.paul@gmail.com>
2017-07-07 09:50:26 -0400

    added reporting function and changed periods from list to dict

--- hello_world.py
+++ hello_world.py
@@ -20,22 +20,24 @@ Years_from_Start_Prompt = "How many years do you want to forecast?"
 Years_from_Start_Error = "Please enter a valid range"
 Years_from_Start = int_input(Years_from_Start_Prompt,Years_from_Start_Error)
 
+Period_Error = "Please enter a valid period type"
+
 Last_Year = Starting_Year + Years_from_Start
 
 #measures of time
 
 Time_Measure_Options = {"annual": 1, "semi-annual": 0.5, "quarter": .25}
 Quarters = {.25: "Q2 ", .5: "Q3 ", .75: "Q4 ", 0.0: "Q1 "}
-Halves = {.5: "H1 ", 0.0: "H2 "}
+Halves = {.5: "H2 ", 0.0: "H1 "}
 
 Time_Measure = raw_input("How do you want to measure periods (quarter, semi-annual, annual?")
 
 while (Time_Measure in Time_Measure_Options) == False:
-	print "Please enter a valid period type"
+	print Period_Error
 	Time_Measure = raw_input("How do you want to measure periods (quarter, semi-annual, annual?")	 
 
 years=[]
-periods = []
+periods = {}
 
 while Starting_Year < Last_Year:
 	#function to add number of years to the list years
@@ -44,12 +46,13 @@ while Starting_Year < Last_Year:
 
 for period in years:
 	if Time_Measure == "quarter":
-		periods.append(str(Quarters[(period%1)]) + str(int(period-(period%1))))
+		periods[(str(Quarters[(period%1)]) + str(int(period-(period%1))))] = period
 	elif Time_Measure == "semi-annual":
-		periods.append(str(Halves[(period%1)]) + str(int(period-(period%1))))
+		periods[(str(Halves[(period%1)]) + str(int(period-(period%1))))] = period
 	else:
-		periods.append("FY "+str(int(period)))
+		periods[("FY "+str(int(period)))] = period
 
+print years #remove after test
 print periods
 
 number_of_periods = len(years)
@@ -69,8 +72,8 @@ Percent_error = "Please enter a valid percent"
 FTE_growth_rate = int_input(FTE_growth_prompt,Percent_error)
 
 FTE_cost_rate_prompt = "What is the annual cost of a FTE?"
-FTE_cost_rate_error = "Please enter a valid number"
-FTE_cost_rate = time_measure_convert(int_input(FTE_cost_rate_prompt,FTE_cost_rate_error))
+Number_error = "Please enter a valid number"
+FTE_cost_rate = time_measure_convert(int_input(FTE_cost_rate_prompt,Number_error))
 
 FTE_cost_rate_growth_prompt = "What is the annual growth rate in FTE costs?"
 FTE_cost_rate_growth = time_measure_convert(int_input(FTE_cost_rate_growth_prompt,Percent_error))
@@ -91,3 +94,31 @@ growth(FTE_cost_rate,FTE_cost_rate_growth,FTE_Cost)
 
 print FTEs
 print FTE_Cost
+
+#operating costs
+
+Operating_costs_prompt = "What is the baseline operating cost amount (annual)?"
+Starting_operating_costs = int_input(Operating_costs_prompt,Number_error)
+
+Operating_costs_rate_prompt = "What rate do basedline costs increase annually?"
+Operating_costs_growth_rate = int_input(Operating_costs_rate_prompt,Percent_error)
+
+Operating_costs ={}
+
+growth(Starting_operating_costs,Operating_costs_growth_rate,Operating_costs)
+
+print Operating_costs
+
+#reporting
+
+period_reported = raw_input("What period do you want financial results for?")
+
+while (period_reported in periods) == False: #needs to be updated to periods; how do I link periods and years lists?
+	print Period_Error
+	period_reported = raw_input("What period do you want financial results for?")
+
+def period_amount(period, numbers):
+	period = periods[period]
+	return numbers[period]
+
+print period_amount(period_reported,FTE_Cost) + period_amount(period_reported,Operating_costs) #ask David - can I create a list with all cost types to make a function vs sum function?
