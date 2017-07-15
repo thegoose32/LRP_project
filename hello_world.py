07030b0154e0c406bb5d6b9b0427c5869efe5441
Paul Giese <giese.paul@gmail.com>
2017-07-15 10:13:33 -0400

    File before removing period labels

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,3 @@
-#input for starting year and how many years out
 
 def int_input(prompt,error_message):
 	#converts raw input from string into integer
@@ -13,12 +12,23 @@ def int_input(prompt,error_message):
 	return Raw_Input_Integer
 
 def count_objects(prompt, error_message,name_objects,name_objects_list):
-	Programs_Count = int_input(prompt,error_message)
+	#uses int_input to convert raw input to integer and then prompts user to name items in a list
+        Count = int_input(prompt,error_message)
 	input_count = 1
-	while input_count < (Programs_Count+1):
+	while input_count < (Count+1):
 		Program_Name = raw_input(name_objects+str(input_count)+"?")  
 		name_objects_list.append(Program_Name)
 		input_count += 1
+
+def yes_no_prompt(prompt):
+    #function that requires a yes or no response
+    yes_no = raw_input(prompt)
+    while (yes_no == "Yes" or "No") == False:
+        print "Please enter a Yes or No response"
+        yes_no = raw_input(prompt)
+    return yes_no
+
+#input for starting year and how many years out
 
 Starting_Year_Prompt = "What year do you want to start forecasting in?"
 Starting_Year_Error = "Please enter a valid year"
@@ -31,7 +41,6 @@ Years_from_Start = int_input(Years_from_Start_Prompt,Years_from_Start_Error)
 Period_Error = "Please enter a valid period type"
 Number_error = "Please enter a valid number"
 Percent_error = "Please enter a valid percent"
-
 
 Last_Year = Starting_Year + Years_from_Start
 
@@ -90,13 +99,58 @@ for program in program_phases:
 
 print program_phases_costs
 
+#Setting up Program Milestones
+#(I think I can create a function to do this and program phases)
+
+revenue_milestones = []
+revenue_milestone_count_prompt = "How many revenue milestones do you want to track?"
+revenue_milestone_label_prompt = "What label do you want for milestone "
+
+count_objects(revenue_milestone_count_prompt,Number_error,revenue_milestone_label_prompt,revenue_milestones)
+
+revenue_milestone_amounts ={}
+
+for milestone in revenue_milestones:
+    revenue_milestone_amount_prompt = "How much revenue is milestone %s worth?" % milestone
+    revenue_milestone_amount = int_input(revenue_milestone_amount_prompt,Number_error)
+    revenue_milestone_amounts[milestone] = revenue_milestone_amount
+
 #Programs
 
 Programs = []
 
 Programs_Count_Prompt = "How many programs does the company have?"
+Program_Name_Prompt = "What name do you want for program "
 
+count_objects(Programs_Count_Prompt, Number_error,Program_Name_Prompt,Programs)
 
+for program in Programs:
+    program_name = program
+    program = {}
+    program_cost = program_name
+    program_costs = {}
+    program_milestone_revenue = {}
+    for period in years:
+        #User assigns what cost phase the program is in by period
+        program_phase_by_period = raw_input("In %(x)s, what phase is program %(y)s in?" % {"x" : period, "y" : program_name})
+        while (program_phase_by_period in program_phases_costs) == False:
+            print "Please enter a valid program cost phase"
+            program_phase_by_period = raw_input("In %(x)s, what phase is program %(y)s in?" % {"x" : period, "y" : program_name})
+        program[period]= program_phase_by_period
+        phase_cost = program_phases_costs[program_phase_by_period]
+        program_costs[period] = phase_cost
+    program_milestone_prompt = "Are there any milestones associated with this program?"
+    program_milestones = yes_no_prompt(program_milestone_prompt)
+    if program_milestones == "Yes":
+        for milestone in revenue_milestones:
+            program_milestone_achieved_prompt = "Is the %(x)s milestone achieved for program %(y)s?" % {"x" : milestone, "y" : program_name}
+            program_milestone_achieved = yes_no_prompt(program_milestone_achieved_prompt)
+            if program_milestone_achieved == "Yes":
+                program_milestone_achieved_period_prompt = "In what period is the %(x)s milestone achieved for program %(y)s?" % {"x" : milestone, "y" : program_name}
+                program_milestone_achieved_period = float(int_input(program_milestone_achieved_period_prompt,Period_Error))
+                program_milestone_achieved_amount = revenue_milestone_amounts[milestone]
+                program_milestone_revenue[program_milestone_achieved_period] = program_milestone_achieved_amount
+    print program_milestone_revenue
 
 #FTEs
 
@@ -117,8 +171,8 @@ FTEs = {}
 
 def growth(base, growth_rate, results):
 	base_period = 0.0
-	for periods in years:
-		results[periods] = int(base*((1+(float(growth_rate)/100))**base_period))
+	for period in years:
+		results[period] = int(base*((1+(float(growth_rate)/100))**base_period))
 		base_period = base_period + Time_Measure_Options[Time_Measure]
 	return results
 
@@ -132,10 +186,10 @@ print FTE_Cost
 
 #operating costs
 
-Operating_costs_prompt = "What is the baseline operating cost amount (annual)?"
+Operating_costs_prompt = "What is the baseline operating cost per %s period?" % Time_Measure
 Starting_operating_costs = int_input(Operating_costs_prompt,Number_error)
 
-Operating_costs_rate_prompt = "What rate do basedline costs increase annually?"
+Operating_costs_rate_prompt = "What rate do baseline costs increase per %s period?" %Time_Measure
 Operating_costs_growth_rate = int_input(Operating_costs_rate_prompt,Percent_error)
 
 Operating_costs ={}
@@ -156,4 +210,15 @@ def period_amount(period, numbers):
 	period = periods[period]
 	return numbers[period]
 
-print period_amount(period_reported,FTE_Cost) + period_amount(period_reported,Operating_costs) #ask David - can I create a list with all cost types to make a function vs sum function?
+print "Total Period Costs:"
+for program in Programs:
+    print "Program %(x)s revenue was %(y)s" % {"x" : program, "y" :period_amount(period_reported,program_milestone_revenue)}
+    print "Program %(x)s costs was %(y)s" % {"x": program, "y" : period_amount(period_reported,program_costs)} 
+    company_revenue = period_amount(period_reported,program_milestone_revenue)
+    company_program_expense = period_amount(period_reported,program_costs)
+
+print "Total FTE costs were %s"  % period_amount(period_reported,FTE_Cost)
+print "Total operating costs were %s" %  period_amount(period_reported,Operating_costs)
+
+net_income = company_revenue - company_program_expense - period_amount(period_reported,FTE_Cost) - period_amount(period_reported,Operating_costs)
+print "The Company's net income was %s" % net_income
